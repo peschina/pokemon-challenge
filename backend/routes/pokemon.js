ca0deaccc10f4d7788df8f9502daf09d74a164a9
Sara Zanellato <szanellato@efebia.com>
2021-06-02 19:19:04 +0200

    Add error handling + more tests

--- backend/routes/pokemon.js
+++ backend/routes/pokemon.js
@@ -5,10 +5,25 @@ const fetch = require("node-fetch");
 
 router.get("/:name", async (req, res) => {
   const name = req.params.name;
+  if (name.length > 32) {
+    res.status(400).send("Name can have 32 char max");
+    return;
+  }
 
   const pokemonRes = await fetch(
     `https://pokeapi.co/api/v2/pokemon-species/${name}`
   );
+
+  if (pokemonRes.status != 200) {
+    res
+      .status(pokemonRes.status)
+      .send(
+        pokemonRes.status == 404
+          ? `There is no pokemon with name ${name}`
+          : "An error occured"
+      );
+    return;
+  }
   const pokemon = await pokemonRes.json();
   const description = pokemon.flavor_text_entries.find(
     i => i.language.name == "en"
--- backend/test/pokemon.test.js
+++ backend/test/pokemon.test.js
@@ -18,4 +18,25 @@ describe("GET /api/pokemon", () => {
       "It can freely recombine its own cellular structure to\ntransform into other life-forms."
     );
   });
+
+  it("should return 404 if pokeman name is not in params", async () => {
+    const res = await request(server).get(`${apiUrl}/`);
+    expect(res.status).toBe(404);
+  });
+
+  it("should return 404 if name param is an empty string", async () => {
+    const res = await request(server).get(`${apiUrl}/${""}`);
+    expect(res.status).toBe(404);
+  });
+
+  it("should return 400 if name has more than 32 char", async () => {
+    const name = new Array(50).join("a");
+    const res = await request(server).get(`${apiUrl}/${name}`);
+    expect(res.status).toBe(400);
+  });
+
+  it("should return 404 if pokeman with given name doesn't exist", async () => {
+    const res = await request(server).get(`${apiUrl}/foo`);
+    expect(res.status).toBe(404);
+  });
 });
